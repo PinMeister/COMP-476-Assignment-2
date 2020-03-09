8aef9d163211a6d4540e961cdab93747796b940d
Patrick Pin <patrickpin840@hotmail.com>
2020-03-09 15:12:12 -0400

    [R2] Add PoVGraph script

---
+++ Assets/Scripts/PoVGraph.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoVGraph : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private float weight(Transform currentNode, Transform adjacentNode)
+    {
+        return (currentNode.position - adjacentNode.position).magnitude;
+    }
+}
